0da2a1656a5037bf727e183bc69ee653982dfb15
Vlad <vladpochta5@gmail.com>
2018-11-03 10:57:43 +0200

    Complete styled components section

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = (): React.Node => (
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Accounts</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <p>Hey!</p>
+  </div>
+);
+
+export default Home;
